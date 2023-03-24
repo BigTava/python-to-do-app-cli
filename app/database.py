d87d0f68b8208e41b1b2eae5b68e5b0485337715
Tiago Tavares <bigtavadev@gmail.com>
2023-03-24 07:53:21 +0000

    feat: app backend

--- app/database.py
+++ app/database.py
@@ -3,7 +3,10 @@
 
 import configparser
 from pathlib import Path
+import json
+from typing import Any, Dict, List, NamedTuple
 
+from app import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR, SUCCESS
 from app import DB_WRITE_ERROR, SUCCESS
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath(
@@ -23,3 +26,31 @@ def init_database(db_path: Path) -> int:
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
+    
+
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:  # Catch wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError:  # Catch file IO problems
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError:  # Catch file IO problems
+            return DBResponse(todo_list, DB_WRITE_ERROR)
